2b580f3384f14f961a0bb1e8324058014562a0cf
Jose Vega <josevega234@gmail.com>
2020-11-17 13:49:30 +0100

    Componentes  detalle

---
+++ src/components/Detail/Button/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles.css";
+
+export const DetailButton = ({ path, content }) => {
+  return (
+    <div className="flex-center">
+      <Link to={path} className="btn btn-info">
+        {content}
+      </Link>
+    </div>
+  );
+};
