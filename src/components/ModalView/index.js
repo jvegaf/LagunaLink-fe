434f01d00589928fda39c46e0fe23775c9cec7ae
Jose Vega <josevega234@gmail.com>
2020-11-18 19:52:32 +0100

    Agregado componente vista modal

---
+++ src/components/ModalView/index.js
@@ -0,0 +1,29 @@
+import { Button, Modal } from 'react-bootstrap';
+import { FcInfo } from "react-icons/fc";
+import React from 'react';
+
+
+export const ModalView  = (props) => {
+  
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+        <FcInfo />
+          Info 
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>{props.message}</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Cerrar</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
