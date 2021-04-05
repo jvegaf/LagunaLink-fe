b70e0a3a092423c64f723f0942fb75fea87c446b
Jose Vega <josevega234@gmail.com>
2021-04-05 13:38:28 +0200

    fetch all companies

--- src/redux/shared.js
+++ src/redux/shared.js
@@ -2,6 +2,7 @@ import { apiProvider } from '../services/api/api-provider';
 
 const initialState = {
   jobOpenings: [],
+  companies: [],
   isBusy: false,
   taskError: false,
   fetched: false
@@ -11,7 +12,9 @@ const initialState = {
 
 
 const FETCH_JOB_OPENINGS = 'FETCH_JOB_OPENINGS';
-const FETCH_COMPLETE = 'FETCH_COMPLETE';
+const FETCH_COMPANIES = 'FETCH_COMPANIES';
+const FETCH_JOBS_COMPLETE = 'FETCH_JOBS_COMPLETE';
+const FETCH_COMPANIES_COMPLETE = 'FETCH_COMPANIES_COMPLETE';
 const FETCH_ERROR = 'FETCH_ERROR';
 
 
@@ -25,10 +28,25 @@ const currentApp = (state = initialState, action) => {
         taskError: false,
       }
 
-    case FETCH_COMPLETE:
+      case FETCH_COMPANIES:
+        return {
+          ...state,
+          isBusy: true,
+          taskError: false,
+        }
+
+    case FETCH_JOBS_COMPLETE:
       return {
         ...state,
         jobOpenings: action.payload,
+        isBusy: false,
+        fetched: true
+      }
+
+    case FETCH_COMPANIES_COMPLETE:
+      return {
+        ...state,
+        companies: action.payload,
         isBusy: false,
         fetched: true
       }
@@ -49,17 +67,24 @@ export default currentApp
 
 // actions
 
-const fetchAllJobOpen = token => (dispatch, getState) => {
+const fetchAllJobOpen = token => dispatch => {
   dispatch({type: FETCH_JOB_OPENINGS})
   apiProvider.getAll('job_openings', token)
-  .then(res => dispatch({type: FETCH_COMPLETE, payload: res.data.jobOpenings}))
+  .then(res => dispatch({type: FETCH_JOBS_COMPLETE, payload: res.data.jobOpenings}))
   .catch(e => dispatch({type: FETCH_ERROR}))
 }
 
+const fetchAllCompanies = token => dispatch => {
+  dispatch({type: FETCH_COMPANIES})
+  apiProvider.getAll('job_openings', token)
+  .then(res => dispatch({type: FETCH_COMPANIES_COMPLETE, payload: res.data.companies}))
+  .catch(e => dispatch({type: FETCH_ERROR}))
+} 
 
 
 
 
 export const actions = {
- fetchAllJobOpen
+ fetchAllJobOpen,
+ fetchAllCompanies
 }
