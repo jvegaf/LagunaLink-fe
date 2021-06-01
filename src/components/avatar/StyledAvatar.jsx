35acef9b931959c279b23e0348e27489072b3b34
Jose Vega <josevega234@gmail.com>
2021-06-01 14:50:50 +0200

    avatar with gradient
    Signed-off-by: Jose Vega <josevega234@gmail.com>

--- src/components/avatar/StyledAvatar.jsx
+++ src/components/avatar/StyledAvatar.jsx
@@ -1,16 +1,17 @@
 import Avatar from '@material-ui/core/Avatar'
-import { useOnlineAvatarStyles } from '@mui-treasury/styles/avatar/online'
+import { useGradientAvatarStyles } from '@mui-treasury/styles/avatar/gradient'
 import React from 'react'
 
 
 export const StyledAvatar = props => {
   const { size, thickness, src } = props
 
-  const styles = useOnlineAvatarStyles({
-    color: '#8d6e63',
+  const styles = useGradientAvatarStyles({
     size: size,
     thickness: thickness,
     gap: 1,
+    gapColor: '#f4f7fa',
+    color: 'linear-gradient(to bottom right, #feac5e, #c779d0, #4bc0c8)',
   })
 
   return (
